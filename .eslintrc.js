9ad70e25299d77cdffc9b479084e63f817a30fb7
Boma <amakirisamuelboma@gmail.com>
2023-06-27 15:04:20 +0100

    CHORE: added precommit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,48 @@
 module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:json/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint'],
   rules: {
+    'eol-last': ['error', 'always'],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': ['error'],
+    '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
+    '@typescript-eslint/quotes': [
+      'warn',
+      'single',
+      {
+        allowTemplateLiterals: true,
+        avoidEscape: true,
+      },
+    ],
+    'json/*': [
+      'error',
+      {
+        allowComments: true,
+      },
+    ],
+    'quote-props': ['error', 'as-needed'],
   },
 };
