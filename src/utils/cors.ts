31566dd74c0f7c845e84d71b9ca2b369f6427f4d
Boma <amakirisamuelboma@gmail.com>
2023-11-13 15:11:23 +0100

    CHORE: update cors

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,4 +1,4 @@
-const prodCors = [];
+const prodCors = ['http://en1tech.com'];
 const devCors = ['http://localhost:3000', 'http://localhost:3001'];
 
 const allowedHosts = process.env.NODE_ENV === 'production' ? prodCors : devCors;
